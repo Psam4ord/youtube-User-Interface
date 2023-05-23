ece580072b60e34765ba4b929ce08191facb74dc
Sammor-Duah Hayford <psam4ord@gmail.com>
2023-05-23 07:53:32 -0700

    setting up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Homepage from './components/homepage';
 
 function App() {
   return (
     <div className="App">
+      <Homepage />
      <h1>Hello Youtube</h1>
     </div>
   );
