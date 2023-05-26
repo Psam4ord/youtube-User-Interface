e311890112b12efcafab6550e32429be380c77ce
Sammor-Duah Hayford <psam4ord@gmail.com>
2023-05-26 09:43:32 -0700

    working on hover effect on navbar

--- src/layouts/navbar.jsx
+++ src/layouts/navbar.jsx
@@ -8,69 +8,86 @@ import Logo from '../commons/logo';
 
 
 const NavBar = () => {
-    const [search, setSearch] = useState(" ")
     const [isHovered, setIsHovered] = useState(false)
 
-    // onhover set to true
-    const handleHover = () => {
-        setIsHovered(true)
-    }
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    };
 
-    // set back to false if mouse leave
-    const onMouseLeave = () => {
+    const handleMouseLeave = () => {
         setIsHovered(false);
     };
 
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        alert('Searching...')
-    }
     return (
         <>
+            <div className=' flex flex-row space-x-4 h-1/4 shadow-md p-3' >
 
-            <div className=' flex flex-row space-x-4 h-1/4 shadow-md p-3 ' >
-                <AiOutlineMenu
-                    className='hover:bg-[#cccccc] hover:rounded-full p-1'
-                    size={40}
+                {/* navbar menu icon */}
+                <span >
+                    <AiOutlineMenu
+                        className='hover:bg-[#cccccc] hover:rounded-full p-1'
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}
+                        size={40} />
+                    {isHovered && <div className="hover:float-right">menu</div>}
+                </span >
 
-                />
 
-                <span>
-                    <Logo />
-                </span>
+                {/* navbar logo icon */}
+                <div>
+                    <Logo
 
-                <span className='flex'>
-                    <input
-                        className='hidden font-poppins border rounded-xl border-[#cccccc]  '
-                        type='text'
-                        placeholder='Search...'
-                        value={search}
-                        onChange={(e) => setSearch(e.target.value)}
                     />
 
-                    <button> < AiOutlineSearch size={40}
-                        className='hover:bg-[#cccccc] hover:rounded-full p-1'
-                    />  </button>
+                </div >
+
+                <span>
+                    <input
+                        className=' hidden font-poppins border rounded-xl border-[#cccccc]'
+                        type='text'
+                        placeholder='Search...'
+                    />
+
+
+                    {/* navbar search bar */}
+                    <button >
+                        < AiOutlineSearch size={40}
+                            className='hover:bg-[#cccccc] hover:rounded-full p-1'
+                            onMouseEnter={handleMouseEnter}
+                            onMouseLeave={handleMouseLeave}
+                        />
+                        {isHovered && <div className="relative  left-0 top-0 right-0 bottom-0" >logo </div>}
+                    </button>
                 </span>
 
+                <span>
+                    {/* navbar-upload-icon */}
 
-                <AiOutlineCloudUpload
-                    className='hover:bg-[#cccccc] hover:rounded-full p-1'
-                    size={40}
-                    onChange={handleHover}
-                />
+                    <AiOutlineCloudUpload
+                        className='hover:bg-[#cccccc] hover:rounded-full p-1'
+                        size={40}
+                       
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}
+                    />
+                     {isHovered && <div className="" >upload </div>}
+                </span>
 
-                <BiVideoPlus
-                    className='hidden hover:bg-[#cccccc] hover:rounded-full p-1'
-                    size={40}
-                />
+                <span>
+                    {/* navbar-add-video-icon */}
+                    <BiVideoPlus
+                        className='hidden hover:bg-[#cccccc] hover:rounded-full p-1'
+                        size={40}
+                    />
+                </span>
 
-
-                <IoMdNotificationsOutline
-                    className='hidden hover:bg-[#cccccc] hover:rounded-full p-1'
-                    size={40} />
-   
+                <span>
+                    {/* navbar-notification-icon */}
+                    <IoMdNotificationsOutline
+                        className='hidden hover:bg-[#cccccc] hover:rounded-full p-1'
+                        size={40} />
+                </span>
 
             </div>
         </>
