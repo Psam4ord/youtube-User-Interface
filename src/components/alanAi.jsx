40907c4fa1ad56a547738682862e08b9f42a58b8
Sammor-Duah Hayford <psam4ord@gmail.com>
2023-06-29 10:42:43 -0700

    fix: Alan textTOSpeech functionality added

--- src/components/alanAi.jsx
+++ src/components/alanAi.jsx
@@ -6,20 +6,28 @@ const AlanAi = () => {
     useEffect(() => {
         alanBtnRef.btnInstance= alanBtn({
             key: 'eb05b0d3608da8151d2992dfc7c9f9252e956eca572e1d8b807a3e2338fdd0dc/stage',
-            onCommand: (commandData) => {
-                if (commandData.command === 'go:back') {
+            onCommand: ({command, text}) => {
+                if (command === 'textToSpeech') {
                     // Call the client code that will react to the received command
-                    console.log("hey there")
+                    textToSpeech(text);
                 }
             }
         });
     }, []);
 
+    // function to handle text-to-speech
+    const textToSpeech = (text) => {
+        // Use a text-to-speech library of your choice to convert text to speech
+        // For example, you can use the Web Speech API
+        const utterance = new SpeechSynthesisUtterance(text);
+        window.speechSynthesis.speak(utterance);
+      };
+
     return (
         <div>
-             <button className='bg-blue-700 text-white rounded-md m-5 p-1' onClick={() => {
+             <button className='bg-blue-700 text-white rounded-md m-5 p-1 justify-' onClick={() => {
         alanBtnRef.btnInstance.playText("Hi there, I am Alan");
-      }}>Play text</button>
+      }}>Play Sound</button>
 
         </div>
     )
